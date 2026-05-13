f893d4fbb19df181256dc215dad5b9d0b1bb34fa
chanweiyan <chanweiyan007@gmail.com>
2026-05-14 00:42:49 +0800

    fix: test mock app scan web qrcode to let user login website

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,8 @@
-import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Headers, Inject, Query, UnauthorizedException } from '@nestjs/common';
 import { AppService } from './app.service';
 import { randomUUID } from 'crypto';
 import * as qrcode from 'qrcode';
+import { JwtService } from '@nestjs/jwt';
 
 // 实际环境使用redis，这里简化处理
 const map = new Map<string, QrCodeInfo>();
@@ -25,6 +26,15 @@ interface QrCodeInfo {
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
+  @Inject(JwtService)
+  private jwtService: JwtService;
+
+  private users = [
+    { id: 1, username: 'foo', password: '111' },
+    { id: 2, username: 'bar', password: '222' },
+  ];
+
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
@@ -47,7 +57,16 @@ export class AppController {
 
   @Get('qrcode/check')
   async check(@Query('id') id: string) {
-    return map.get(`qrcode_${id}`);
+    const info = map.get(`qrcode_${id}`)!;
+    if (info.status === 'scan-confirm') {
+      return {
+        token: await this.jwtService.sign({
+          userId: info.userInfo?.userId
+        }),
+        ...info
+      }
+    }
+    return info;
   }
 
   @Get('qrcode/scan')
@@ -62,12 +81,24 @@ export class AppController {
   }
 
   @Get('qrcode/confirm')
-  async confirm(@Query('id') id: string) {
+  async confirm(@Query('id') id: string, @Headers('Authorization') auth: string) {
+    let user;
+    try {
+      const [, token] = auth.split(' ');
+      const info = await this.jwtService.verify(token);
+
+      user = this.users.find(item => item.id == info.userId);
+    } catch (e) {
+      throw new UnauthorizedException('token 过期，请重新登录');
+    }
+    console.log('confirm id', id, user)
+
     const info = map.get(`qrcode_${id}`);
     if (!info) {
       throw new BadRequestException('二维码已过期');
     }
     info.status = StatusEnum.ScanConfirm;
+    info.userInfo = user;
     return 'success';
   }
 
@@ -81,4 +112,35 @@ export class AppController {
     return 'success';
   }
 
+  @Get('login')
+  async login(@Query('username') username: string, @Query('password') password: string) {
+
+    const user = this.users.find(item => item.username === username);
+
+    if (!user) {
+      throw new UnauthorizedException('用户不存在');
+    }
+    if (user.password !== password) {
+      throw new UnauthorizedException('密码错误');
+    }
+
+    return {
+      token: await this.jwtService.sign({
+        userId: user.id
+      })
+    }
+  }
+
+  @Get('userInfo')
+  async userInfo(@Headers('Authorization') auth: string) {
+    try {
+      const [, token] = auth.split(' ');
+      const info = await this.jwtService.verify(token);
+
+      const user = this.users.find(item => item.id == info.userId);
+      return user;
+    } catch (e) {
+      throw new UnauthorizedException('token 过期，请重新登录');
+    }
+  }
 }
