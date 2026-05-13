edf68043fb052ec6f60cc71981f187a8be765281
chanweiyan <chanweiyan007@gmail.com>
2026-05-13 23:44:19 +0800

    fix: implement /qrcode/generate api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { randomUUID } from 'crypto';
+import * as qrcode from 'qrcode';
 
 @Controller()
 export class AppController {
@@ -10,5 +12,14 @@ export class AppController {
     return this.appService.getHello();
   }
 
-
+  @Get('qrcode/generate')
+  async generateQRCode() {
+    const uuid = randomUUID();
+    const url = `http://192.168.1.2:3000/pages/confirm.html?id=${uuid}`;
+    const dataUrl = await qrcode.toDataURL(url);
+    return {
+      qrcode_id: uuid,
+      img: dataUrl,
+    }
+  }
 }
