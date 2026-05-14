7249e155cb5e71fb01d7c52ff1dc7a9c1ac63c7d
chanweiyan <chanweiyan007@gmail.com>
2026-05-14 09:50:38 +0800

    fix: create aaa, bbb resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JwtModule } from '@nestjs/jwt';
+import { AaaModule } from './aaa/aaa.module';
+import { BbbModule } from './bbb/bbb.module';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: 'foobar'
-    })
+    }),
+    AaaModule,
+    BbbModule
   ],
   controllers: [AppController],
   providers: [AppService],
