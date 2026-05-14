9bc5fc9a93d319864aa33bb69af29defd5938703
chanweiyan <chanweiyan007@gmail.com>
2026-05-14 17:52:26 +0800

    fix: test os api and node-dist-info pkg

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,8 @@ import { AppService } from './app.service';
 import { randomUUID } from 'crypto';
 import * as qrcode from 'qrcode';
 import { JwtService } from '@nestjs/jwt';
+import * as os from 'os';
+import * as nodeDiskInfo from 'node-disk-info';
 
 // 实际环境使用redis，这里简化处理
 const map = new Map<string, QrCodeInfo>();
@@ -150,4 +152,94 @@ export class AppController {
   async universityList() {
     return this.appService.getUniversityData();
   }
+
+  @Get('status')
+  async status() {
+    return {
+      cpu: this.getCpuInfo(),
+      mem: this.getMemInfo(),
+      dist: await this.getDiskStatus(),
+      sys: this.getSysInfo()
+    }
+  }
+
+  getSysInfo() {
+    return {
+      computerName: os.hostname(),
+      computerIp: this.getServerIP(),
+      osName: os.platform(),
+      osArch: os.arch(),
+    };
+  }
+
+  getServerIP() {
+    const nets = os.networkInterfaces();
+    for (const name of Object.keys(nets)) {
+      for (const net of nets[name] || []) {
+        if (net.family === 'IPv4' && !net.internal) {
+          return net.address;
+        }
+      }
+    }
+  }
+
+
+  async getDiskStatus() {
+    const disks = await nodeDiskInfo.getDiskInfoSync();
+
+    const sysFiles = disks.map((disk: any) => {
+      return {
+        dirName: disk._mounted,
+        typeName: disk._filesystem,
+        total: this.bytesToGB(disk._blocks) + 'GB',
+        used: this.bytesToGB(disk._used) + 'GB',
+        free: this.bytesToGB(disk._available) + 'GB',
+        usage: ((disk._used / disk._blocks || 0) * 100).toFixed(2),
+      };
+    });
+    return sysFiles;
+  }
+
+
+  bytesToGB(bytes) {
+    const gb = bytes / (1024 * 1024 * 1024);
+    return gb.toFixed(2);
+  }
+
+  getMemInfo() {
+    const totalMemory = os.totalmem();
+    const freeMemory = os.freemem();
+    const usedMemory = totalMemory - freeMemory;
+    const memoryUsagePercentage = (((totalMemory - freeMemory) / totalMemory) * 100).toFixed(2);
+    const mem = {
+      total: this.bytesToGB(totalMemory),
+      used: this.bytesToGB(usedMemory),
+      free: this.bytesToGB(freeMemory),
+      usage: memoryUsagePercentage,
+    };
+    return mem;
+  }
+
+  getCpuInfo() {
+    const cpus = os.cpus();
+    const cpuInfo = cpus.reduce(
+      (info, cpu) => {
+        info.cpuNum += 1;
+        info.user += cpu.times.user;
+        info.sys += cpu.times.sys;
+        info.idle += cpu.times.idle;
+        info.total += cpu.times.user + cpu.times.sys + cpu.times.idle;
+        return info;
+      },
+      { user: 0, sys: 0, idle: 0, total: 0, cpuNum: 0 },
+    );
+    const cpu = {
+      cpuNum: cpuInfo.cpuNum,
+      sys: ((cpuInfo.sys / cpuInfo.total) * 100).toFixed(2),
+      used: ((cpuInfo.user / cpuInfo.total) * 100).toFixed(2),
+      free: ((cpuInfo.idle / cpuInfo.total) * 100).toFixed(2),
+    };
+    return cpu;
+  }
+
 }
